90aa285c289b979180c6d04667caaf8ccb461a78
Musa Elashaal <elashaalmusa@gmail.com>
2025-07-03 13:10:44 -0400

    Update features to include hoep

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -1,4 +1,3 @@
-
 import pandas as pd
 import numpy as np
 
@@ -15,15 +14,15 @@ def create_features(df, lags=[1, 2, 3], roll_windows=[3]):
     df['hour_cos'] = np.cos(2 * np.pi * df['hour'] / 24)
     df.drop(columns=['hour'], inplace=True)
 
-    #  Lag features 
+    # Lag features 
     for lag in lags:
-        df[f'HOEP_lag_{lag}'] = df['HOEP'].shift(lag)
         df[f'Demand_lag_{lag}'] = df['Ontario Demand'].shift(lag)
-       
+        df[f'HOEP_lag_{lag}'] = df['HOEP'].shift(lag)
 
-    #  Rolling features 
+    # Rolling features 
     for win in roll_windows:
         df[f'Demand_ma_{win}'] = df['Ontario Demand'].rolling(window=win).mean()
+        df[f'HOEP_ma_{win}'] = df['HOEP'].rolling(window=win).mean()
 
     # Drop rows with any NA created by shift/rolling
     df = df.dropna()
